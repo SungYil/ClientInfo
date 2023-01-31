d44fb2258ce5adecad6b77523845ae73b3e79339
ParkSungYil <shfur2006@gmail.com>
2023-02-01 00:11:09 +0900

    Initial commit

---
+++ src/main/java/clientinfo/controllers/ClientController.java
@@ -0,0 +1,24 @@
+package clientinfo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ClientController{
+    @RequestMapping(value = "/getip", method = RequestMethod.GET)
+    public String home(Model model){
+        HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String ip = req.getHeader("X-FORWARDED-FOR");
+        if (ip == null)
+            ip = req.getRemoteAddr();
+
+        model.addAttribute("clientIP", ip);
+        return "home";
+    }
+}
