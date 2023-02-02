42d82e27ee1a4663ccd32c772a99a1b38da56676
SungYil <shfur2006@gmail.com>
2023-02-02 18:03:48 +0900

    ADD get header info api

--- src/main/java/clientinfo/controllers/ClientController.java
+++ src/main/java/clientinfo/controllers/ClientController.java
@@ -1,24 +1,38 @@
 package ClientInfo.controllers;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class ClientController{
     @RequestMapping(value = "getip", method = RequestMethod.GET)
     public String home(Model model){
+
         HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         String ip = req.getHeader("X-FORWARDED-FOR");
+
+        System.out.println(req.getHeader("user-agent"));
         if (ip == null)
             ip = req.getRemoteAddr();
 
         model.addAttribute("clientIP", ip);
         return ""+ip;
     }
+
+    @RequestMapping(value = "getheader")
+    public String gethead(@RequestHeader Map<String, String> headers){
+        
+        for(Map.Entry<String, String> entry : headers.entrySet()){
+            System.out.println("key : "+entry.getKey()+" , value : "+entry.getValue());
+        }
+        return "success";
+    } 
 }
